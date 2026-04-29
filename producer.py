1ab35b5465039b55616696b486804e1f333a833e
sustagantos <gusan.sp@gmail.com>
2026-04-29 14:50:08 -0300

    finished setting up kafka producer, broker and consumer

--- producer.py
+++ producer.py
@@ -13,13 +13,15 @@ def delivery_report(err,msg):
     if err:
         print(f"Delivery error: {err}")
     else:
-        print(f"Delivery successfull: {msg.value().decode("utf-8")}")
+        print(f"Delivered {msg.value().decode("utf-8")}")
+        print(f"Delived to {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}")
+        #print(dir(msg))
 
 order = {
     "order_id": str(uuid.uuid4()),
-    "user": "gugu",
-    "item": "beer",
-    "quantity": 100
+    "user": "julia",
+    "item": "frozen yogurt",
+    "quantity": 10
 }
 
 value = json.dumps(order).encode("utf-8")
